01fc644dcdd7832f7dfa8493e1c2c24ced42b353
UsefSalim <salimguithub@gmail.com>
2021-03-23 14:32:15 -0300

    separation des routes retest heroku

--- go.README.md
+++ go.README.md
@@ -1,3 +1,7 @@
 // configuraion server 
 // add Procfile file 
 // add version node to package json 
+// heroku login 
+// heroku create ucefecommerceapi
+// heroku push origin main 
+// heroku open 
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 require('dotenv').config({ path: './config/.env' });
 // packages
 const express = require('express');
-
+const testRoutes = require('./routes/test.routes')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Routes
-app.use("/",(req,res)=>{
-  res.status(200).json('test deployment')
-})
+app.use("/",testRoutes)
 // app express
 app.listen(PORT, () => {
   console.log(`app listning : localhost:${PORT}`);
