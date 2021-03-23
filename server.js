9317a2620a177d49df42021d38ee59d6dcb9ad7f
UsefSalim <salimguithub@gmail.com>
2021-03-23 14:44:52 -0300

    add mongoose connection and retest heroku deployment

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 require('dotenv').config({ path: './config/.env' });
 // packages
 const express = require('express');
+const mongoose = require('mongoose');
 const testRoutes = require('./routes/test.routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Mongo Db Connected'))
+  .catch((err) => console.log(`error connection to the DataBase : ${err}`));
 // Routes
 app.use('/api', testRoutes);
 // app express
