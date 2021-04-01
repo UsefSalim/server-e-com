3295f88d6f8990a012db904eab8094cbfd07beb8
UsefSalim <salimguithub@gmail.com>
2021-04-01 10:01:49 -0300

    disable auth roots

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth.routes')
+// const authRoutes = require('./routes/auth.routes')
 const categorieRoutes = require('./routes/Categorie.routes');
-// const productRoutes = require('./routes/Product.routes');
+const productRoutes = require('./routes/Product.routes');
 
 const app = express();
 
@@ -23,9 +23,9 @@ mongoose
   .then(() => console.log('Mongo Db Connected ....'))
   .catch((err) => console.log(`Error connection to the DataBase : ${err}`));
 // Routes
-app.use('/api', authRoutes)
+// app.use('/api', authRoutes)
 app.use('/api/categories', categorieRoutes);
-// app.use('/api/products', productRoutes);
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () =>
 {
